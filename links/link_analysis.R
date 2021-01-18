dffa5033ac07428c799be95e5488512e1d9a49db
jimmyjhickey <jimmyjhickey@gmail.com>
2021-01-17 19:11:55 -0500

    Added dates sent to links and Rmd table

--- links/link_analysis.R
+++ links/link_analysis.R
@@ -4,6 +4,8 @@ library(rvest)
 
 full_data = read.csv("newsletters/csv/intermediate/all_months_201920_dens.csv")
 
+# filter for only opportunities 
+# combine opportunity/1234 and opportunity/1234#tab-****
 opp_data = full_data %>% 
                 select("link") %>%
                 mutate_all(funs(str_replace(.,"#.*", ""))) %>%
@@ -16,15 +18,31 @@ length(unique(opp_data$link))
 link_df = as.data.frame(sort(table(opp_data$link), decreasing= TRUE))
 names(link_df) = c("link", "click count")
 link_df$title = NA
+link_df$dates_sent = NA
 
+
+# get title for each url
 for(row in 1:nrow(link_df)){
   url_string = toString(link_df$link[row])
-  # print(url_string)
-  
+
   url = html(url_string)
   link_df$title[row] = url %>% 
-                        html_node("strong") %>% 
-                        html_text()
+            html_node("strong") %>% 
+            html_text()
 }
 
+# get dates sent for each url
+for(row in 1:nrow(link_df)){
+  # get date sent
+  date = full_data %>% 
+    filter(grepl(link_df$link[row],link)) %>%
+    select(date_sent)
+  
+  link_df$dates_sent[row] = unique(date)
+  link_df$dates_sent[row][[1]] = sort(link_df$dates_sent[row][[1]])
+}
+
+
 save(link_df, file="link_df.RData")
+
+

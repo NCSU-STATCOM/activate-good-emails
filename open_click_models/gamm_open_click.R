061dd3df115d1f6324f7a575873936dd8fe04d97
AlvinSheng <asheng1067@gmail.com>
2021-02-09 08:29:52 -0600

    constructing bam model

--- open_click_models/gamm_open_click.R
+++ open_click_models/gamm_open_click.R
@@ -1,1 +1,21 @@
 
+library(mgcv)
+library(itsadug)
+
+load("open_click_models/subscriber_open.RData")
+
+
+
+ptm <- proc.time()
+
+m1 <- bam(week_open ~ covid + 
+            s(date_sent, by = covid) + 
+            s(mins_since_midnight, by = covid) + 
+            s(subject_length) + 
+            s(date_sent, subscriberid, bs = "fs", m = 1), 
+          family = "binomial",
+          data = subscriber_open)
+
+save(m1, file = "open_click_models/m1.RData")
+
+proc.time() - ptm

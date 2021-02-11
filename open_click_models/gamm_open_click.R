def3d162f5fcbf313517bde7d0f2b2aeb3836086
AlvinSheng <asheng1067@gmail.com>
2021-02-11 17:17:07 -0600

    trying out several models. Models m0, m4, m6 have finished within a reasonable time.

--- open_click_models/gamm_open_click.R
+++ open_click_models/gamm_open_click.R
@@ -1,6 +1,8 @@
 
 library(mgcv)
 library(itsadug)
+library(forecast)
+library(tidymv)
 
 load("open_click_models/subscriber_open.RData")
 
@@ -60,3 +62,111 @@ m2 <- bam(week_open ~ covid +
 save(m2, file = "open_click_models/m2.RData")
 
 proc.time() - ptm
+
+
+
+ptm <- proc.time()
+
+m3 <- bam(week_open ~ covid + 
+            s(days_since_start, by = covid) + 
+            s(mins_since_midnight, by = covid) + 
+            s(subject_length) + 
+            s(subscriberid, bs = "re"), 
+          family = "binomial",
+          data = subscriber_open, 
+          discrete = TRUE)
+
+save(m3, file = "open_click_models/m3.RData")
+
+proc.time() - ptm
+
+
+
+# trying to subset the data to speed things up
+
+subid <- unique(subscriber_open$subscriberid)
+
+set.seed(232)
+
+subid_sample <- subid[sample(1:length(subid), size = length(subid) / 10)]
+
+dat_sample <- subscriber_open[subscriber_open$subscriberid %in% subid_sample,]
+
+
+
+ptm <- proc.time()
+
+m4 <- bam(week_open ~ covid + 
+            s(days_since_start, by = covid) + 
+            s(mins_since_midnight, by = covid) + 
+            s(subject_length) + 
+            s(subscriberid, bs = "re"), 
+          family = "binomial",
+          data = dat_sample, 
+          discrete = TRUE)
+
+save(m4, file = "open_click_models/m4.RData")
+
+proc.time() - ptm
+
+
+
+ptm <- proc.time()
+
+m5 <- bam(week_open ~ covid + 
+            s(days_since_start, by = covid) + 
+            s(mins_since_midnight, by = covid) + 
+            s(subject_length) + 
+            s(days_since_start, subscriberid, bs = "fs", m = 1), 
+          family = "binomial",
+          data = dat_sample, 
+          discrete = TRUE)
+
+save(m5, file = "open_click_models/m5.RData")
+
+proc.time() - ptm
+
+# R-sq.(adj) =  0.412
+
+
+
+ptm <- proc.time()
+
+m6 <- bam(week_open ~ covid + s
+            s(days_since_start, by = covid) + 
+            s(mins_since_midnight, by = covid) + 
+            s(subject_length) + 
+            s(subscriberid, bs = "re") +
+            s(subscriberid, days_since_start, bs = "re"), 
+          family = "binomial",
+          data = dat_sample, 
+          discrete = TRUE)
+
+save(m6, file = "open_click_models/m6.RData")
+
+proc.time() - ptm
+
+# R-sq.(adj) =  0.429 
+
+
+
+#######
+
+load("~/Documents/NCSU_Spring_2021/activate_good_project/open_click_models/m4.RData")
+
+
+
+# There doesn't seem to be severe autocorrelation in time. 
+
+# checkresiduals(m4)
+
+
+
+
+
+
+
+
+
+
+
